dad686c8dde04746fae387466390acfd88590abe
NL33 <stkellman@gmail.com>
2015-02-25 10:09:24 -0500

    initial parse backend setup and log in screen

--- SnapchatClone/AppDelegate.swift
+++ SnapchatClone/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  SnapchatClone
 //
-//  Created by Sean on 2/25/15.
+//  Created by NL33 on 2/25/15.
 //  Copyright (c) 2015 NL33. All rights reserved.
 //
 
@@ -10,37 +10,77 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    /* NOTE: Changed
+    
+    application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+    
+    to
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+    
+    */
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        
+        
+        Parse.setApplicationId("XCIhN3zN7xZIZ3e0yEUJtRZaeseugO2ynPiDjPsn", clientKey: "zjpDi1QI9mbyaoM0oWFXm69Pg8TAruxvMIZCW1Cc")
+        
+        
         return true
     }
-
+    
+    // Changed UIApplication! to UIApplication
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
+    // Changed UIApplication! to UIApplication
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
+    // Changed UIApplication! to UIApplication
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
+    
+    // Changed UIApplication! to UIApplication
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    /*  Changed
+    
+    func application(application: UIApplication, openURL url: NSURL,
+    sourceApplication: NSString, annotation: AnyObject) -> Bool {
+    
+    to
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+    
+    */
+    
+    //func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        
+        //return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication,
+         //   withSession:PFFacebookUtils.session())
+    //}
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        
+      //  FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
+        
+    }
+    
+    
+    
 }
-
--- SnapchatClone/ViewController.swift
+++ SnapchatClone/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var username: UITextField!
+    
+    
+    @IBAction func signIn(sender: AnyObject) {
+        
+        PFUser.logInWithUsernameInBackground(username.text, password:"mypass") { //tries to log user in automatically when they hit sign up. We are not requiring a password, so just here enter "mypass" as password
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                
+                println("logged in")
+                
+                self.performSegueWithIdentifier("showUsers", sender: self)
+                
+            } else { //if it does not work to log them in, they we will try and sign them up:
+                
+                var user = PFUser()
+                user.username = self.username.text
+                user.password = "mypass"
+                
+                
+                user.signUpInBackgroundWithBlock {
+                    (succeeded: Bool!, error: NSError!) -> Void in
+                    if error == nil {
+                        
+                        println("signed up")
+                        
+                        self.performSegueWithIdentifier("showUsers", sender: self)
+                        
+                    } else { //if that is not successful, then we have an error:
+                        
+                        println(error)
+                    }
+                }
+                
+                
+            }
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        if PFUser.currentUser() != nil {
+            
+            self.performSegueWithIdentifier("showUsers", sender: self)
+            
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
